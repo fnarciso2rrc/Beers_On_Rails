c656271b48209206fe4b82c2690a1d546f164cad
Frans Narciso <fransnarc@gmail.com>
2024-10-09 16:14:28 -0500

    Added validations to the Beer and Company

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,2 +1,6 @@
 class Beer < ApplicationRecord
+    belongs_to :company
+
+    validates :name, :price, :description, prescence: true
+    validares :price, numericality: true
 end
--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,6 @@
 class Company < ApplicationRecord
+    has_many :beers
+
+
+    validates :name, presence: true
 end
