c3fcd725d0335d861bdf92f7d44ee681c96ba35a
Frans Narciso <fransnarc@gmail.com>
2024-10-10 21:08:22 -0500

    Added a very simple unstyled nav.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :beers
 
-  resources :company, only: %i[index show]
+  resources :companies, only: %i[index show]
 
   root to: "companies#index"
  #  get '/companies', to: 'companies#index', as: 'companies'
